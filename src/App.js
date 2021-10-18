a5c29a382d5f71bfdf7014b2908f6eb40a9b9f69
Liudmyla Kozachenko <viergee@gmail.com>
2021-10-19 01:18:56 +0300

    add save contacts in local storage

--- src/App.js
+++ src/App.js
@@ -15,7 +15,19 @@ class App extends Component {
     ],
     filter: '',
   };
-
+  componentDidMount() {
+    const localContacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(localContacts);
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  componentWillUnmount() {}
   addNewContact = contact => {
     this.setState(prevState => {
       if (prevState.contacts.some(item => item.name.includes(contact.name))) {
